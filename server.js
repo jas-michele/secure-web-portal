2174f5625da08d2272ae92e081cba6683769bb0d
Jasmine <jasmineoliver158@gmail.com>
2026-05-23 12:46:08 -0400

    Implement user registration endpoint with email uniqueness check

--- server.js
+++ server.js
@@ -8,15 +8,40 @@ const PORT = process.env.PORT;
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
+const User = require('./models/userModel');
 
+app.post("/test", async (req, res) => {
+     try {
 
-app.use("/", (req, res) => {
-    res.send("Teasting...")
+        const {username, email, password} = req.body;
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists"
+            })
+        }
+
+        const newUser = await User.create({
+            username,
+            email,
+            password
+        });
+
+        res.status(201).json({
+            _id: newUser._id,
+            username: newUser.username,
+            email: newUser.email
+        });
+    } catch (error) {
+        console.log(error)
+
+        res.status(500).json({
+            message: error.message
+        })
+    }
 })
-
-
-
-
 
 
 
