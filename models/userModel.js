9b029e8b5b982f47b840cfe18c62636a8ffd6a5c
Jasmine <jasmineoliver158@gmail.com>
2026-05-23 12:08:16 -0400

    Implement user schema with password hashing and comparison methods; add initial route and utility files

--- models/userModel.js
+++ models/userModel.js
@@ -20,3 +20,18 @@ const userSchema = new Schema({
         minlength: 5
     },
 })
+
+userSchema.pre('save', async function() {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds)
+    }
+})
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+}
+
+const User = model('User', userSchema);
+
+module.exports = User;
