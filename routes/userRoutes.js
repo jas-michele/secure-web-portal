9b850e7845736dd36a1ca9e3a6b01040426234f5
Jasmine <jasmineoliver158@gmail.com>
2026-05-24 12:11:38 -0400

    Refactor user registration logic and remove test endpoint; add username uniqueness check

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,11 +4,37 @@ const { signToken } = require('../utils/auth');
 
 router.post('/register', async (req, res) => {
     try {
-        const user = await User.create(req.body);
+        const { username, email, password} = req.body;
+
+        const existingUser = await User.findOne({ email });
+
+        const existingUsername = await User.findOne({ username });
+
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists"
+            })
+        }
+
+        if (existingUsername) {
+            return res.status(400).json({
+                message: "Username already exists"
+            })
+        }
+
+        const user = await User.create({
+            username,
+            email,
+            password
+        });
+
         const token = signToken(user);
+
         res.status(201).json({ token, user });
     } catch (error) {
         console.log(error)
         res.status(400).json(error);
     }
 })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,43 +5,14 @@ require('dotenv').config();
 require('./config/db')
 const PORT = process.env.PORT;
 
+const userRoutes = require('./routes/userRoutes')
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
 const User = require('./models/userModel');
 
-app.post("/test", async (req, res) => {
-     try {
-
-        const {username, email, password} = req.body;
-
-        const existingUser = await User.findOne({ email });
-
-        if (existingUser) {
-            return res.status(400).json({
-                message: "User already exists"
-            })
-        }
-
-        const newUser = await User.create({
-            username,
-            email,
-            password
-        });
-
-        res.status(201).json({
-            _id: newUser._id,
-            username: newUser.username,
-            email: newUser.email
-        });
-    } catch (error) {
-        console.log(error)
-
-        res.status(500).json({
-            message: error.message
-        })
-    }
-})
+app.use('/api/users', userRoutes)
 
 
 
